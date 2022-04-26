b3f7c177e1c3ac17170e25d7ed90aa9f29978665
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-04-26 12:55:15 +0300

    BasePage and MainPage are created. Test for main page is written.

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
+    """Testing that user can open login page"""
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    main_page = MainPage(browser, link)     # инициализируем PageObject
+    main_page.open()    # открываем главную страницу, функция наследована от класса MainPage
+    main_page.go_to_login_page()    # переходим на страницу регистрации
