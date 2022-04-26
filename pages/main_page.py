3f2ca52dd64e3c5ab93547eb7d10f575f3df4bcc
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-04-26 14:31:36 +0300

    new locators added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,13 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         """Open link in browser"""
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
         """Login link should be visible"""
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not visible"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not visible"
--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,9 @@ from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     """Testing that user can open login page"""
     link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)     # инициализируем PageObject
-    main_page.open()    # открываем главную страницу, функция наследована от класса MainPage
-    main_page.go_to_login_page()    # переходим на страницу регистрации
+    page = MainPage(browser, link)     # инициализируем PageObject
+    page.open()    # открываем главную страницу, функция наследована от класса MainPage
+    page.go_to_login_page()    # переходим на страницу регистрации
 
 
 def test_guest_should_see_login_link(browser):
