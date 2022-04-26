b2adf455b875a91ae6a3af4ef1f783568674c479
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-04-26 20:29:13 +0300

    added new methods for testing product page and basket

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REG_FORM = (By.CSS_SELECTOR, ".register_form")
+
+
+class ProductPageLocators:
+    ADD_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ITEM_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    ITEM_NAME_IN_BASKET = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    ITEM_COST = (By.CSS_SELECTOR, ".product_main > .price_color")
+    ITEM_COST_IN_BASKET = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,37 @@
+import math
+from selenium.common.exceptions import NoAlertPresentException
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        """Add item to basket"""
+        self.browser.find_element(*ProductPageLocators.ADD_BASKET_BUTTON).click()
+
+    def check_item_name(self):
+        """Checking name and cost of item in basket"""
+        name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        name_in_basket = self.browser.find_element(*ProductPageLocators.ITEM_NAME_IN_BASKET).text
+        assert name == name_in_basket, "Names of item not equal"
+
+    def check_item_cost(self):
+        """Checking name and cost of item in basket"""
+        cost = self.browser.find_element(*ProductPageLocators.ITEM_COST).text
+        cost_in_basket = self.browser.find_element(*ProductPageLocators.ITEM_COST_IN_BASKET).text
+        assert cost == cost_in_basket, "Costs of item not equal"
+
+    def solve_quiz_and_get_code(self):
+        """Math formula for calculation and send answer"""
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
