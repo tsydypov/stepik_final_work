ec24496ebbe06906d01a7faac6a17e5c5210cfa5
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-04-26 14:46:17 +0300

    login page methods used in main page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,14 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     """Testing that user can open login page"""
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)     # инициализируем PageObject
-    page.open()    # открываем главную страницу, функция наследована от класса MainPage
     page.go_to_login_page()    # переходим на страницу регистрации
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
