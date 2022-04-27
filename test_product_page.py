c7f0f2f8a003abaf2644788f401836b347352d2d
Цыдыпов Владимир <vov_sed@mail.rugit config --global user.name Vladimir>
2022-04-27 16:51:46 +0300

    mark.parametrize added to the test for link

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_code', [0, 1, 2, 3, 4, 5, 6,
+                                        pytest.param(7, marks=pytest.mark.xfail),
+                                        8, 9])
+def test_guest_can_add_product_to_basket(browser, promo_code):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_code}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
